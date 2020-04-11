1a7a14bdabd85525d8002a246ad6c13a9a17eda3
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-11 05:47:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 # if the provided key is present, increment its value by 1
 def update_counting_hash(hash, key)
   if != hash
-    hash[key] += 1
+    hash[key] = 1
   else
     hash[key] += 1
   end
