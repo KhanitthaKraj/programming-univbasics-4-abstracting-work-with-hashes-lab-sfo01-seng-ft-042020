a7e87257b710f44ee04892a3660527e65877b7ea
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-11 05:51:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 # return a hash that includes the key and value parameters passed into this method
 def my_hash_creator(key, value)
-  {key => value}
+  { key => value }
 end
 
 # returns the value corresponding to the provided key
